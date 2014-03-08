ebcc3a0c26422b2cd9980f1171b7869a9f2fa0ea
Janus Varmarken <varmarken@gmail.com>
2014-03-08 13:38:01 +0100

    Added project for creating JCAF monitors for BLIP devices.

---
+++ assignment1/BLIPDeviceMonitoring/src/dk/itu/spct/f2014/pmor/janv/ma01/context/monitoring/BLIPDeviceMonitor.java
@@ -0,0 +1,30 @@
+package dk.itu.spct.f2014.pmor.janv.ma01.context.monitoring;
+
+import java.rmi.RemoteException;
+
+import dk.pervasive.jcaf.util.AbstractMonitor;
+
+public class BLIPDeviceMonitor extends AbstractMonitor {
+
+	/**
+	 * Generated serial version UID.
+	 */
+	private static final long serialVersionUID = -1318891287337945814L;
+
+	public BLIPDeviceMonitor(String service_uri) throws RemoteException {
+		super(service_uri);
+	}
+
+	@Override
+	public void monitor(String arg0) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
