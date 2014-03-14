6abd6c197a899bac6eb2f40452a40389566b6132
Janus Varmarken <varmarken@gmail.com>
2014-03-14 13:12:30 +0100

    Created Android project for the Android App part of mandatory asssignment 01.

---
+++ assignment1/AndroidApp/gen/dk/itu/spct/f2014/ma01/pmor/janv/androidapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package dk.itu.spct.f2014.ma01.pmor.janv.androidapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
