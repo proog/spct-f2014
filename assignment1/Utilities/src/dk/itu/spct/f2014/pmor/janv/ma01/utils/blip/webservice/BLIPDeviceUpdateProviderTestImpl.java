cc2d77f50d8e8768b65221ca1430d7676f09b067
Janus Varmarken <varmarken@gmail.com>
2014-03-16 11:08:39 +0100

    Added test implementation of IBLIPDeviceUpdateProvider

---
+++ assignment1/Utilities/src/dk/itu/spct/f2014/pmor/janv/ma01/utils/blip/webservice/BLIPDeviceUpdateProviderTestImpl.java
@@ -0,0 +1,39 @@
+package dk.itu.spct.f2014.pmor.janv.ma01.utils.blip.webservice;
+
+import java.io.IOException;
+
+import dk.itu.spct.f2014.pmor.janv.ma01.utils.blip.model.BLIPDevice;
+
+/**
+ * An implementation of {@link IBLIPDeviceUpdateProvider} that clients of
+ * IBLIPDeviceUpdateProvider can use for testing. This is an example of how the
+ * used dependency injection pattern enhances testability.
+ * 
+ * @author Janus Varmarken
+ * 
+ */
+public class BLIPDeviceUpdateProviderTestImpl implements
+		IBLIPDeviceUpdateProvider<BLIPDevice> {
+
+	private final BLIPDevice device = new BLIPDevice();
+
+	private final String[] testLocations = new String[]{ "itu.zone1", "itu.zone2", "itu.zone3", "itu.zone4", "LOCATION_UNKNOWN" };
+	
+	private int count = 0;
+	
+	public BLIPDeviceUpdateProviderTestImpl() {
+		this.device.setTerminalId("123456789123");
+		this.device.setLastEventDescription("test");
+		this.device.setLastEventTimestamp(42L);
+		this.device.setMajorClassOfDevice("artificial");
+		
+	}
+
+	@Override
+	public BLIPDevice getDeviceUpdate(String deviceId) throws IOException {
+		this.device.setLocation(this.testLocations[count]);
+		count++;
+		// TODO add (modulo) check on count, i.e. keep cycling?
+		return this.device;
+	}
+}
