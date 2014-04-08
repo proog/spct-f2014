4b59e8c658b937ac9f2cf20523963d04cfeaea2b
Janus Varmarken <varmarken@gmail.com>
2014-04-08 12:39:53 +0200

    cleanup

--- assignment2/SurfaceApp/SurfaceApp/SurfaceWindow1.xaml.cs
+++ assignment2/SurfaceApp/SurfaceApp/SurfaceWindow1.xaml.cs
@@ -1,39 +1,19 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Microsoft.Surface;
-using Microsoft.Surface.Presentation;
-using Microsoft.Surface.Presentation.Controls;
-using Microsoft.Surface.Presentation.Input;
-using SurfaceApp.Network;
 using System.Collections.ObjectModel;
 using System.Drawing;
+using Microsoft.Surface;
+using Microsoft.Surface.Presentation.Controls;
+using SurfaceApp.Network;
 
 namespace SurfaceApp
 {
     /// <summary>
-    /// Interaction logic for SurfaceWindow1.xaml
+    ///     Interaction logic for SurfaceWindow1.xaml
     /// </summary>
     public partial class SurfaceWindow1 : SurfaceWindow, IImageObserver
     {
-
-        public ObservableCollection<Image> ImagesInScatterView
-        {
-            get;
-            private set;
-        }
-
         /// <summary>
-        /// Default constructor.
+        ///     Default constructor.
         /// </summary>
         public SurfaceWindow1()
         {
@@ -53,13 +33,16 @@ namespace SurfaceApp
 			SignalR.GetInstance().Start();
 		}
 
+        public ObservableCollection<Image> ImagesInScatterView { get; private set; }
+
+
         public void ImageAdded(Image newImage)
         {
             ImagesInScatterView.Add(newImage);
         }
 
         /// <summary>
-        /// Occurs when the window is about to close. 
+        ///     Occurs when the window is about to close.
         /// </summary>
         /// <param name="e"></param>
         protected override void OnClosed(EventArgs e)
@@ -73,7 +56,7 @@ namespace SurfaceApp
         }
 
         /// <summary>
-        /// Adds handlers for window availability events.
+        ///     Adds handlers for window availability events.
         /// </summary>
         private void AddWindowAvailabilityHandlers()
         {
@@ -84,7 +67,7 @@ namespace SurfaceApp
         }
 
         /// <summary>
-        /// Removes handlers for window availability events.
+        ///     Removes handlers for window availability events.
         /// </summary>
         private void RemoveWindowAvailabilityHandlers()
         {
@@ -95,7 +78,7 @@ namespace SurfaceApp
         }
 
         /// <summary>
-        /// This is called when the user can interact with the application's window.
+        ///     This is called when the user can interact with the application's window.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -105,7 +88,7 @@ namespace SurfaceApp
         }
 
         /// <summary>
-        /// This is called when the user can see but not interact with the application's window.
+        ///     This is called when the user can see but not interact with the application's window.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -117,7 +100,7 @@ namespace SurfaceApp
         }
 
         /// <summary>
-        /// This is called when the application's window is not visible or interactive.
+        ///     This is called when the application's window is not visible or interactive.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -128,7 +111,7 @@ namespace SurfaceApp
 
         private void TagVisualizer_VisualizationAdded(object sender, TagVisualizerEventArgs e)
         {
-            var visualizer = (TagVisualizer)sender;
+            var visualizer = (TagVisualizer) sender;
             var visualization = (PhoneVisualization) e.TagVisualization;
             long tagVal = visualization.VisualizedTag.Value;
             // TODO call some sort of onTagAdded(tagVal) event handler
