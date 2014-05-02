81ca2941328ced28f69d6cf71ab5a23cbc9f4f45
Per Mortensen <per@permortensen.com>
2014-05-02 13:49:30 +0200

    Google App Engine project should work now.

--- assignment3/MovementServlet/src/dk/itu/spct/f2014/ma03/pmor/janv/servlet/MovementServletServlet.java
+++ assignment3/MovementServlet/src/dk/itu/spct/f2014/ma03/pmor/janv/servlet/MovementServletServlet.java
@@ -2,59 +2,76 @@ package dk.itu.spct.f2014.ma03.pmor.janv.servlet;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.*;
 
+import org.apache.tools.ant.taskdefs.Filter;
+
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
 import com.google.appengine.api.datastore.*;
+import com.google.appengine.api.datastore.Query.CompositeFilter;
+import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.google.appengine.api.datastore.Query.SortDirection;
 
 @SuppressWarnings("serial")
 public class MovementServletServlet extends HttpServlet {
-	private Entity parent = new Entity("Measurement", "Measurement"); 
+	private Entity trainingRecording = new Entity("TrainingRecording", "TrainingRecording");
+	private Entity testRecording = new Entity("TestRecording", "TestRecording");
+	private String uploadName = "data";
+	private String modeName = "mode";
+	private String idName = "id";
+	private String trainingMode = "training";
+	private String testMode = "test";
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-		Query query = new Query("Measurement").addSort("timestamp", SortDirection.DESCENDING);
-		List<Entity> list = datastore.prepare(query).asList(FetchOptions.Builder.withDefaults());
 		
-		String output = "<!DOCTYPE html><html><body>";
-		
-		if (!list.isEmpty()) {
-			output = output + "<table>";
+		String id = req.getParameter(idName);
+		if(id != null && !id.isEmpty()) {
+			Query q = new Query("TestRecording").setFilter(new FilterPredicate(idName, FilterOperator.EQUAL, id));
+			List<Entity> list = datastore.prepare(q).asList(FetchOptions.Builder.withDefaults());
 			
-			for (Entity e : list) {
-				output = output + "<tr><td>" + e.getProperty("timestamp").toString() + "</td><td>"
-											+ e.getProperty("elemvalue").toString() + "</td></tr>";
-			}
-			
-			output = output + "</table>";
+			for(Entity e : list)
+				resp.getWriter().println(e.getProperty(uploadName));
 		}
 		else {
-			output = output + "<p>Database Empty</p>";
+			Query q = new Query("TrainingRecording");
+			List<Entity> list = datastore.prepare(q).asList(FetchOptions.Builder.withDefaults());
+			
+			int count = 0;
+			for(Entity e : list) {
+				String data = e.getProperty(uploadName).toString();
+				String[] lines = data.split("\n");
+				int start = count == 0 ? 0 : 1;
+				for(int i = start; i < lines.length; i++)
+					resp.getWriter().println(lines[i]);
+				count++;
+			}
 		}
-	    
-		output = output + "</body></html>";
-	    
-		resp.setContentType("text/html");
-	    resp.getWriter().println(output);
 	}
 
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		String timestamp = req.getParameter("timestamp");
-		String type = req.getParameter("type");
-		String xAxis = req.getParameter("x_acc");
-		String yAxis = req.getParameter("y_acc");
-		String zAxis = req.getParameter("z_acc");
-		
-		Entity measurement = new Entity("Measurement", parent.getKey());
-		measurement.setProperty("timestamp", timestamp);
-		measurement.setProperty("type", type);
-		measurement.setProperty("x_acc", xAxis);
-		measurement.setProperty("y_acc", yAxis);
-		measurement.setProperty("z_acc", zAxis);
-		
 		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-		datastore.put(measurement);
+		
+		String mode = req.getParameter(modeName);
+		Entity e;
+		if(mode.equals(testMode)) {
+			e = new Entity("TestRecording", testRecording.getKey());
+		}
+		else {
+			e = new Entity("TrainingRecording", trainingRecording.getKey());
+		}
+		
+		String data = req.getParameter(uploadName);
+		e.setProperty("id", req.getParameter(idName));
+		e.setProperty("data", data);
+		datastore.put(e);
 	}
 	
 }
